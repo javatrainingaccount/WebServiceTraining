d253ff1e7244b6460a85e7cc40ec31dc06444bb3
Raja <rajavijayakanth.b@tyfone.com>
2019-12-24 20:03:32 +0530

    deleted

--- trainingProject3/src/main/java/com/example/trainingProject3/Q.java
+++
@@ -1,9 +0,0 @@
-package com.example.trainingProject3;
-
-public class Q {
-
-	public void show(String message) {
-		System.out.println("<html><head></head><body>Hai</body></html>");
-	}
-
-}
--- trainingProject3/src/test/java/com/example/trainingProject3/ClassLoaderExample.java
+++
@@ -1,9 +0,0 @@
-package com.example.trainingProject3;
-
-public class ClassLoaderExample {
-
-	public void getClassLoaderName() {
-		ClassLoader cl = Thread.currentThread().getContextClassLoader();
-		System.out.println("class: " + cl.getClass() + ", parent: " + cl.getParent()+", cl: "+cl);
-	}
-}
